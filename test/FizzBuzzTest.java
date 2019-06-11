fe81344ba0d01491c3c8ac97e9e56fb3474e767e
KhanhNguyen206 <nguyen.ngockhanh206@gmail.com>
2019-06-11 14:07:04 +0700

    Refactor Var

---
+++ test/FizzBuzzTest.java
@@ -0,0 +1,12 @@
+
+class FizzBuzzTest {
+    public static void main(String[] args) {
+        System.out.println(FizzBuzz.fizzBuzz(10));
+        System.out.println(FizzBuzz.fizzBuzz(15));
+        System.out.println(FizzBuzz.fizzBuzz(9));
+        System.out.println(FizzBuzz.fizzBuzz(20));
+        System.out.println(FizzBuzz.fizzBuzz(7));
+        System.out.println(FizzBuzz.fizzBuzz(8));
+        System.out.println(FizzBuzz.fizzBuzz(11));
+    }
+}
